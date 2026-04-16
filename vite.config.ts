c4398a3167ae499d6c70b1e717d9296e7f23b447
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-04-16 22:58:13 +0300

    Enable HTTPS dev server with mkcert and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+
+console.log("Vite config loaded");
 
 export default defineConfig({
+  plugins: [mkcert()],
   server: {
-    host: true
-  }
+    host: true,
+    https: true,
+    strictPort: true,
+    port: 5173,
+  },
 });
