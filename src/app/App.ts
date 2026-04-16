a4eae26b3c51fd5947025b9c2afec47889c3759b
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-04-16 21:57:13 +0300

    Wire gesture controller

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,11 +1,13 @@
 import { CameraManager } from "../camera/CameraManager";
 import { MarkerTracker } from "../tracking/MarkerTracker";
 import { SceneManager } from "../scene/SceneManager";
+import { GestureController } from "../interaction/GestureController";
 
 export class App {
   private cameraManager = new CameraManager();
   private tracker = new MarkerTracker();
   private scene!: SceneManager;
+  private gestures = new GestureController();
 
   async start(container: HTMLElement) {
     const video = await this.cameraManager.start();
@@ -14,13 +16,16 @@ export class App {
     this.scene = new SceneManager(container);
     this.scene.start();
 
+    // attach gestures to user-controlled group
+    this.gestures.attach(this.scene.getAnchor().userGroup, container);
+
     this.tracker.onPose((pose) => {
       this.scene.updatePose(pose);
     });
 
     await this.tracker.start();
 
-    // TEMP: simulate pose for now
+    // TEMP: simulate pose
     setInterval(() => {
       this.tracker["emitPose"]?.({
         position: [0, 0, -1],
